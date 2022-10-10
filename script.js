580bb2b1a7257afe21e387afe5cc04e8116edac3
alexanderjsn <alexanderjsn@live.com>
2022-10-10 04:14:37 +0000

    added function that retarts game after 10 points

--- script.js
+++ script.js
@@ -23,10 +23,11 @@ function rollNumber() {
     document.getElementsByClassName("pscore")[0].innerHTML = pscore;
     document.getElementsByClassName("oscore")[0].innerHTML = oscore;
 
+    //alerts the player upon victory/defeat//
     if(pscore == 10) {
-        document.getElementsByClassName("result")[0].innerHTML = "Congratulations, you won!";
+        document.getElementsByClassName("result")[0].innerHTML = "Congratulations, you won!"; pscore = 0; oscore = 0;
     } else if(oscore == 10) {
-        document.getElementsByClassName("result")[0].innerHTML = "Too bad, you lost!";
+        document.getElementsByClassName("result")[0].innerHTML = "Too bad, you lost!"; pscore = 0; oscore = 0;
     }
 
 
